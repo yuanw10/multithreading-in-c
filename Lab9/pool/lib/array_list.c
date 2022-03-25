db34823bcced3f1bbb48e9cb144e915f34875fec
Yuan Wang <wang7@timberlea.CS.Dal.Ca>
2022-03-25 11:55:06 -0300

    Updated lib of pool

--- Lab9/pool/lib/array_list.c
+++ Lab9/pool/lib/array_list.c
@@ -1,199 +1,226 @@
-//The code is from previous labs that I wrote
-//Author: me - Yuan Wang
-//Date accessed: March 24, 2022
+//The code below is from the lab solutions written by our TA
+//Author: Robert Smith 
+//Accessed On: March 25, 2022
+//Link: https://git.cs.dal.ca/courses/2022-winter/csci-2122/solutions/-/blob/main/Lab8/heap/lib/array_list.c
+
 
 #include "array_list.h"
-#include <string.h>
-#include <stdbool.h>
+
 #include <stdlib.h>
+#include <string.h>
 
-ArrayList* alist_initialize(int maxNum, int typeSize, char* typeName)
+ArrayList* alist_initialize(int size, int typeSize, char* typeName)
 {
-  //Allocate memory for the list
-  ArrayList* list = malloc(sizeof(*list));
+	// Create the memory for holding the array list.
+	ArrayList* alist = malloc(sizeof(*alist));
 
-  //Initialize values
-  list -> maxSize = maxNum;
-  list -> itemSize = typeSize;
+	// If we couldn't allocate enough memory, return here.
+	if(alist == NULL)
+		return NULL;
 
-  list -> type = malloc(sizeof(char) * strlen(typeName));
-  strcpy(list -> type, typeName);
+	// Initialize the starting size and set the counts.
+	alist->arr  = malloc(sizeof(*(alist->arr)) * size);
+	alist->size = 0;
+	alist->maxSize = size;
+	alist->itemSize = typeSize;
 
-  list -> size = 0;
-  list -> arr = malloc(maxNum * sizeof(void*));
-
-  return list;
+	// Allocate memory for the type name.
+	alist->type = malloc(strlen(typeName)+1);
+	
+	// Set the type name for reference purposes.
+	strcpy(alist->type, typeName);
+	
+	// Return the array list structure.
+	return alist;
 }
 
-
-
-bool alist_add(ArrayList* list, void* elem)
+bool alist_add(ArrayList* alist, void* element)
 {
-  if(list == NULL || elem == NULL)
-    return false;
+	// If the incoming array list is null, we can return
+	// false here. The element is checked in "add_at".
+	if(alist == NULL)
+		return false;
 
-
-  //If max size reached, resize the array
-  if(list -> size == list -> maxSize){
-    if(!_alist_resize(list))
-      return false;
-  }
-
-  //Allocate memory for the new element
-  list->arr[list->size] = malloc(list->itemSize);
-
-  //Insert the element
-  memcpy(list->arr[list->size], elem, list->itemSize);
-
-  //Increase the current size
-  list -> size ++;
-
-  return true;
+	// Attempt to add the element to the end of the list.
+	return alist_add_at(alist, alist->size, element);
 }
 
-
-
-bool alist_add_at(ArrayList* list, int index, void* elem)
+bool alist_add_at(ArrayList* alist, int index, void* element)
 {
-  if(list == NULL || elem == NULL || index > list->size || index < 0)
-    return false;
+	// Check if the incoming pointers are null. If so, return false.
+	if(alist == NULL || element == NULL)
+		return false;
 
+	// We can't add at an index that isn't yet initialized.
+	if(index < 0 || index > alist->size)
+		return false;
 
-  //If max size reached, resize the array
-  if(list -> size == list -> maxSize){
-    if(!_alist_resize(list))
-      return false;
-  }
+	// If we have no room for the new element, increase
+	// the size of this array list by double.
+	if(alist->size == alist->maxSize)	
+	{
+		// Attempt to resize the list.
+		bool success = _alist_resize(alist);
 
-  //Shift elements on the right of the index to the right by one position
-  for(int i = list->size - 1; i >= index; i--)
-  {
-    list->arr[i+1] = list->arr[i];
-  }
+		// If the list failed to resize, return false.
+		if(!success)
+			return false;
+	}
+	
+	// Move all of the elements after the given index
+	// one space to the right to make room.
+	for(int i=alist->size; i > index; i--)
+		alist->arr[i] = alist->arr[i-1];
+	
+	// Allocate memory to the void pointer at the given index.
+	alist->arr[index] = malloc(alist->itemSize);
 
-  //Allocate memory for the new element
-  list->arr[index] = malloc(list->itemSize);
+	// Add this value to the end of the list.
+	memcpy(alist->arr[index], element, alist->itemSize);
 
-  //Insert the element
-  memcpy(list->arr[index], elem, list->itemSize);
+	// Increase the size by 1.
+	alist->size++;
 
-  //Increase the current size
-  list -> size ++;
-
-  return true;
-  
+	// Since we didn't fail earlier, we succeed here.
+	return true;
 }
 
-
-
-void alist_clear(ArrayList* list)
+void alist_clear(ArrayList* alist)
 {
-  //Free every element on the list
-  for(int i = 0; i < list->size; i++)
-  {
-    free(list->arr[i]);
-  }
-
-  //Set current size to zero
-  list->size = 0;
+	// Set every element in maxSize to null.
+	for(int i=0; i < alist->maxSize; i++)
+	{
+		free(alist->arr[i]);
+		alist->arr[i] = NULL;
+	}
+	
+	// Set the size to 0
+	alist->size = 0;
 }
 
-
-
-void* alist_get(ArrayList* list, int index)
+void* alist_get(ArrayList* alist, int index)
 {
-  //If the index out of range or list null, return null
-  if(list == NULL || index > list->size || index < 0)
-    return NULL;
+	// Check if the array list is null. If yes, return false.
+	if(alist == NULL)
+		return false;
 
-
-  return list->arr[index];
+	// If the index doesn't exist in the bounds, return null.
+	if(index < 0 || index >= alist->size)
+		return NULL;
+		
+	// Since the index has to exist, we can return it.
+	return alist->arr[index];
 }
 
-
-int alist_index_of(ArrayList* list, void* elem)
+int alist_index_of(ArrayList* alist, void* element)
 {
-  //If empty list or element null, return -1
-  if(list == NULL || elem == NULL)
-    return -1;
+	// Check if the incoming pointers are null. If yes, return -1.
+	if(alist == NULL || element == NULL)
+		return -1;
 
-  //Compare every element, if equal, return the index
-  for(int i = 0; i < list->size; i++)
-  {
-    if(!memcmp(elem, list->arr[i], list->itemSize))
-      return i;
-  }
+	// Iterate through the whole array if necessary.
+	for(int i=0; i < alist->size; i++)
+	{
+		// Compare the current item to the incoming element.
+		// If the elements match, return i.
+		if(memcmp(alist->arr[i], element, alist->itemSize) == 0)
+			return i;
+	}
 
-  //Otherwise, return -1
-  return -1;
+	// If we got here, then the element wasn't in the
+	// list, so we can return -1.
+	return -1;
 }
 
-
-void* alist_remove(ArrayList* list, int index)
+void* alist_remove(ArrayList* alist, int index)
 {
-  //If empty list or index out of range, return null
-  if(list == NULL || index < 0 || index > list->size - 1)
-    return NULL;
+	// Check if the array list is null. If yes, return NULL.
+	if(alist == NULL)
+		return NULL;
 
-  //Store the element for returning later
-  void* elem = list->arr[index];
+	// If the index isn't in the bounds of initialized values, return null.
+	if(index < 0 || index >= alist->size)
+		return NULL;
+	
+	// Save the element being removed.
+	void* element = malloc(alist->itemSize);
+	memcpy(element, alist->arr[index], alist->itemSize);	
 
+	// Free the data in the memory location.
+	free(alist->arr[index]);
 
-  //Shift every element right of the index to the left by one position
-  for(int i = index + 1; i < list->size; i++)
-  {
-    list->arr[i - 1] = list->arr[i];
-  }
+	// Start at the provided index and iterate to the end of the list-1 .
+	// At each step, move the index into the current index position.
+	for(int i=index; i < alist->size-1; i++)
+		alist->arr[i] = alist->arr[i+1];
 
+	// Set the current last index to null.
+	alist->arr[alist->size-1] = NULL;
 
-  //Decrease the current size
-  list->size--;
-  
-  //Erase the value at the end of the list
-  list->arr[list->size] = NULL;
+	// Set the new size value.
+	alist->size--;
 
-  return elem;
+	// Return the removed element.
+	return element;
 }
 
-
-bool alist_destroy(ArrayList* list)
+bool alist_swap(ArrayList* alist, int a, int b)
 {
-  //If empty list, nothing to destroy
-  if(list == NULL) return false;
+	// Check if the array list is null. If yes, return false.
+	if(alist == NULL)
+		return false;
+	
+	// If a and b are the same, then the array list is in the
+	// correct state, so we can return true.
+	if(a == b)
+		return true;
+	
+	// If a doesn't fit inside the list size, return false.
+	if(a < 0 || a >= alist->size)
+		return false;
+	
+	// If b doesn't fit inside the list size, return false.
+	if(b < 0 || b >= alist->size)
+		return false;
 
-  //Deallocate elements on the list
-  for(int i = 0; i < list->size; i++)
-  {
-    free(list->arr[i]);
-  }
+	// Use a three-point value rotation to swap the indexes.
+	void* temp = alist->arr[a];
+	alist->arr[a] = alist->arr[b];
+	alist->arr[b] = temp;
 
-
-  //Deallocate the type and the list itself
-  free(list->type);
-  free(list);
-
-
-  return true;
+	// We can successfully return.
+	return true;
 }
 
-
-bool _alist_resize(ArrayList* list) 
+bool alist_destroy(ArrayList* alist)
 {
-  //If empty list or not yet reached max size, return false
-  if(list == NULL || list-> size < list->maxSize) 
-    return false;
+	// Clear the array list. This also deallocates all elements.
+	alist_clear(alist);
 
+	// Free the type string.
+	free(alist->type);
 
-  //Increase the max size by twice
-  list->maxSize *= 2;
+	// Free the array list.
+	free(alist);
 
-  //Re-allocate memory for the array
-  list->arr = realloc(list->arr, sizeof(void*) * list->maxSize);
-
-
-  return true;
+	// Return true when everything is deallocated.
+	return true;
 }
 
+bool _alist_resize(ArrayList* alist)
+{	
+	// Multiply the maximum size by 2.
+	alist->maxSize *= 2;
 
+	// Reallocate the memory block to be the new maximum size.
+	alist->arr = realloc(alist->arr, alist->maxSize * sizeof(*(alist->arr)));
+
+	// If the new memory block is null, we return false.
+	if(alist->arr == NULL)
+		return false;
+
+	// Return true if we get here.
+	return true;
+}
 
 
--- Lab9/pool/lib/array_list.h
+++ Lab9/pool/lib/array_list.h
@@ -1,42 +1,33 @@
-//The code is from previous labs that I wrote
-//Author: me - Yuan Wang
-//Date accessed: March 24, 2022
+//The code below is from the lab solutions written by our TA
+//Author: Robert Smith 
+//Accessed On: March 25, 2022
+//Link: https://git.cs.dal.ca/courses/2022-winter/csci-2122/solutions/-/blob/main/Lab8/heap/lib/array_list.h
 
 
-#ifndef __ARRAY_LIST
-#define __ARRAY_LIST
-
-#include <string.h>
+#ifndef __ARRAYLIST_HEADER
+#define __ARRAYLIST_HEADER
 #include <stdbool.h>
-#include <stdlib.h>
 
 typedef struct _AList
 {
-  void** arr;
-  int size;
-  int maxSize;
-  int itemSize;
-  char* type;
+	void** arr;
+	int size;
+	int maxSize;
+	int itemSize;	
+	char* type;	
 } ArrayList;
 
-
 ArrayList* alist_initialize(int, int, char*);
-bool alist_add(ArrayList*, void*);
-bool alist_add_at(ArrayList*, int, void*);
-void alist_clear(ArrayList*);
-void* alist_get(ArrayList*, int);
-int alist_index_of(ArrayList*, void*);
-void* alist_remove(ArrayList*, int);
-bool alist_destroy(ArrayList*);
-bool _alist_resize(ArrayList*);
+bool       alist_add(ArrayList*, void*);
+bool       alist_add_at(ArrayList*, int, void*);
+void       alist_clear(ArrayList*);
+void*      alist_get(ArrayList*, int);
+int        alist_index_of(ArrayList*, void*);
+void*      alist_remove(ArrayList*, int);
+bool       alist_swap(ArrayList*, int, int);
+bool       alist_destroy(ArrayList*);
 
-
-
-
+bool 	   _alist_resize(ArrayList*);
 
 #endif
-
-
-
-
 
